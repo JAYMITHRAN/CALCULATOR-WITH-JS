75dbeefb132e1fbaaec79973471e0ceb119c1805
JAYMITHRAN <149167084+JAYMITHRAN@users.noreply.github.com>
2024-04-05 11:42:36 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var  input = document.getElementById('dodo');
+	function core(num)
+	{ 
+	   dodo.value+=num;
+	   
+	}
+	function code_red()
+	{
+	 dodo.value=eval(dodo.value);
+	}
+	function destroy()
+	{
+	 dodo.value="";
+	}
